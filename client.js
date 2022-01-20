b4aaf377cfe026dd3b9f46b067ea517c6242a561
Brandon <brandon.dahlberg21@gmail.com>
2022-01-20 17:33:08 +0000

    Cleaned up my files

--- client.js
+++ client.js
@@ -3,21 +3,29 @@ const net = require('net');
 // establishes a connection with the game server
 const connect = function () {
 	const conn = net.createConnection({
-		host: '165.227.47.243',
-		port: 50541,
+		host: 'localhost',
+		port: 3000,
+	});
+	conn.on('connect', data => {
+		console.log('server says: ', data);
+		conn.write('Name: BMD');
+		setTimeout(() => {
+			conn.write('Move: up');
+			setTimeout(() => {
+				conn.write('Move: up');
+				setTimeout(() => {
+					conn.write('Move: up');
+				}, 150);
+			}, 100);
+		}, 50);
 	});
 
 	// interpret incoming data as text
 	conn.setEncoding('utf8');
 
-	conn.on('data', data => {
-		console.log('Server says: ', data);
-	});
-	conn.on('connect', () => {
-		conn.write('Hello from client!');
-	});
-
 	return conn;
 };
 console.log('Connecting ...');
 connect();
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,45 +1,7 @@
 const net = require('net');
-
-// establishes a connection with the game server
-const connect = function () {
-	const conn = net.createConnection({
-		host: 'localhost',
-		port: 3000,
-	});
-	conn.on('connect', data => {
-		console.log('server says: ', data);
-		conn.write('Name: BMD');
-		setTimeout(() => {
-			conn.write('Move: up');
-			setTimeout(() => {
-				conn.write('Move: up');
-				setTimeout(() => {
-					conn.write('Move: up');
-				}, 150);
-			}, 100);
-		}, 50);
-	});
-
-	// interpret incoming data as text
-	conn.setEncoding('utf8');
-
-	return conn;
-};
-
-const setupInput = function () {
-	const stdin = process.stdin;
-	stdin.setRawMode(true);
-	stdin.setEncoding('utf8');
-	stdin.resume();
-	stdin.on('data', handleUserInput);
-	return stdin;
-};
-
-const handleUserInput = function (key) {
-	if (key === '\u0003') {
-		process.exit();
-	}
-};
+const { connect } = require('./client');
+const { setupInput } = require('./input');
+const { builtinModules } = require('module');
 
 console.log('Connecting ...');
 
