ff296c35c91f5ac0382fbb87951cc6022c65874d
Brandon <brandon.dahlberg21@gmail.com>
2022-01-20 23:30:01 +0000

    Made constants file

--- client.js
+++ client.js
@@ -1,10 +1,10 @@
 const net = require('net');
-
+const { PORT, IP } = require('./constants');
 // establishes a connection with the game server
 const connect = function () {
 	const conn = net.createConnection({
-		host: 'localhost',
-		port: 3000,
+		host: IP,
+		port: PORT,
 	});
 	conn.on('connect', data => {
 		console.log('server says: ', data);
